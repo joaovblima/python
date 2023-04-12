836fd3275ab9189a4a34a08fac42e01e9de1a825
João Victor Bernardino de Lima <124071979+joaovblima@users.noreply.github.com>
2023-04-12 09:46:39 -0300

    Update exe058.py

--- exe058.py
+++ exe058.py
@@ -1,8 +1,3 @@
-'''058: Melhore o jogo do DESAFIO 0288 onde o computador vai
-''pensar'' em um numero entre 0 e 10. Só que agora o jogador
-vai tentar adivinhar até acertar, mostrando no final quantos
-palpites foram necessarios para vencer.'''
-
 from random import randint
 
 pc = randint(0,10)
