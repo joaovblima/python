0b53358d22e1502400b0a5e1ffb961c0852d88b6
João Victor Bernardino de Lima <124071979+joaovblima@users.noreply.github.com>
2023-04-12 09:45:51 -0300

    Update exe057.py

--- exe057.py
+++ exe057.py
@@ -1,8 +1,3 @@
-'''057: Faça um programa que leia o sexo de uma pessoa, mas
-só aceite os valores 'M' ou 'F'. Caso esteja errado, peça
-a digitação novamente até ter o valor correto.
-'''
-
 sexo = str(input("Digite seu sexo [M/F]:"))
 while sexo != 'M' and sexo !='F':
     sexo = (input("Dados inválidos. Digite seu sexo novamente [M/F]:"))
